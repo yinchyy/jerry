e1cd1dad83aae624e553219c31b0486267e14347
Szymon Studniarz <szymek1019@gmail.com>
2022-12-06 17:29:28 +0100

    display created components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Search } from "./components/Search";
+import { Table } from "./components/Table";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      </header>
+    <div className="flex min-h-screen min-w-full bg-blue-0">
+      <div className="container px-6">
+        <p className="text-base font-bold text-anthracite-100">Characters</p>
+        <Search />
+        <Table />
+      </div>
     </div>
   );
 }
