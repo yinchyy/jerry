b912ea546d1bb2d60b91d8ef52ad0efdfe00d1f8
Szymon Studniarz <szymek1019@gmail.com>
2022-12-08 20:15:54 +0100

    prepare table for pagination, add sample pagination

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { SetStateAction, useEffect } from "react";
 import {
   createColumnHelper,
   flexRender,
   getCoreRowModel,
+  getPaginationRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 import { Character } from "../../types";
@@ -39,15 +41,28 @@ const columns = [
 export const Table = ({
   data,
   loading,
+  totalPages,
+  setCurrent,
 }: {
   data: Character[] | undefined;
   loading: boolean;
+  totalPages: number;
+  setCurrent: React.Dispatch<SetStateAction<number>>;
 }) => {
   const table = useReactTable({
     data: data ? data : [],
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    manualPagination: true,
+    pageCount: totalPages,
   });
+  useEffect(() => {
+    table.setPageSize(5);
+  }, []);
+  useEffect(() => {
+    setCurrent(() => table.getState().pagination.pageIndex + 1);
+  }, [table.getState().pagination.pageIndex]);
   if (loading) {
     return <div>data loading...</div>;
   }
@@ -98,6 +113,55 @@ export const Table = ({
           ))}
         </tfoot>
       </table>
+      <div className="flex items-center gap-2">
+        <button
+          className="rounded border p-1"
+          onClick={() => table.setPageIndex(0)}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {"<<"}
+        </button>
+        <button
+          className="rounded border p-1"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {"<"}
+        </button>
+        <button
+          className="rounded border p-1"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          {">"}
+        </button>
+        <button
+          className="rounded border p-1"
+          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+          disabled={!table.getCanNextPage()}
+        >
+          {">>"}
+        </button>
+        <span className="flex items-center gap-1">
+          <div>Page</div>
+          <strong>
+            {table.getState().pagination.pageIndex + 1} of{" "}
+            {table.getPageCount()}
+          </strong>
+        </span>
+        <span className="flex items-center gap-1">
+          | Go to page:
+          <input
+            type="number"
+            defaultValue={table.getState().pagination.pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              table.setPageIndex(page);
+            }}
+            className="rounded w-16 border p-1"
+          />
+        </span>
+      </div>
     </div>
   );
 };
