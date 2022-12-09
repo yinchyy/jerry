bf8cc70bf4509f517b7592f8dfec838f5a5944f5
Szymon Studniarz <szymek1019@gmail.com>
2022-12-09 10:08:09 +0100

    add arrow icons for pages navigation

--- src/components/Table/icons/index.tsx
+++ src/components/Table/icons/index.tsx
@@ -1,3 +1,5 @@
 export { ReactComponent as Alive } from "./alive.svg";
 export { ReactComponent as Unknown } from "./unknown.svg";
 export { ReactComponent as Dead } from "./dead.svg";
+export { ReactComponent as ArrowLeft } from "./arrowLeft.svg";
+export { ReactComponent as ArrowRight } from "./arrowRight.svg";
